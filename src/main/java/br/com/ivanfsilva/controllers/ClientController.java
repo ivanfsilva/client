b08c04eedd5310e379e410892200e0493b5d266b
ivanfsilva <ivanfs27@gmail.com>
2021-05-22 12:25:18 -0300

    insert implemented

--- src/main/java/br/com/ivanfsilva/controllers/ClientController.java
+++ src/main/java/br/com/ivanfsilva/controllers/ClientController.java
@@ -1,6 +1,6 @@
 package br.com.ivanfsilva.controllers;
 
-import javax.websocket.server.PathParam;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,9 +9,12 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.ivanfsilva.dto.ClientDTO;
 import br.com.ivanfsilva.services.ClientService;
@@ -42,6 +45,15 @@ public class ClientController {
 		
 		return ResponseEntity.ok().body(dto);
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
