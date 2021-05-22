8c38bbe47ab97a5d6a05bedd1e7317f0e3ea0e74
ivanfsilva <ivanfs27@gmail.com>
2021-05-22 12:48:47 -0300

    delete client implemented

--- src/main/java/br/com/ivanfsilva/services/ClientService.java
+++ src/main/java/br/com/ivanfsilva/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import br.com.ivanfsilva.dto.ClientDTO;
 import br.com.ivanfsilva.entities.Client;
 import br.com.ivanfsilva.repositories.ClientRepository;
+import br.com.ivanfsilva.services.exceptions.DatabaseException;
 import br.com.ivanfsilva.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -62,6 +65,16 @@ public class ClientService {
 			return new ClientDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("id não encontrado " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("id não encontrado " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integridade violada");
 		}
 	}
 
